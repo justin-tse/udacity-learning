a0f4643badd7a8cba6dffac13227071b3119fa5b
justin-tse <xiefahit@gmail.com>
2020-12-09 18:52:26 +0800

    Finish L1 variables and data types problem set

---
+++ ud282-javaProgrammingBasics/L1_problem_set.java
@@ -0,0 +1,18 @@
+int bankBalance = 500;
+bankBalance = bankBalance + 250;
+bankBalance = bankBalance + 100;
+System.out.println(bankBalance);
+
+int day = 18;
+String month = "Jan.";
+
+String firstName = "Justin";
+String lastName = "Tse";
+String fullName = firstName + ' ' + lastName;
+int number = firstName.length() + lastName.length();
+System.out.println("Hello, my name is "+fullName+'./n'
++"There are "+number+" letters in my name.");
+
+double fahrenheit = 18.0;
+double celsius = (fahrenheit - 32) * 5 / 9;
+System.out.println(celsius);
