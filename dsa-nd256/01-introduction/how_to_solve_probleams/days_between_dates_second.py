0a453c6cbf4aed91237feb09a265a10671a3889c
justin-tse <xiefahit@gmail.com>
2020-12-12 09:52:31 +0800

    First rule: What are the inputs

--- dsa-nd256/01-introduction/how_to_solve_probleams/days_between_dates_second.py
+++ dsa-nd256/01-introduction/how_to_solve_probleams/days_between_dates_second.py
@@ -1,6 +1,14 @@
 # Days Between Dates
 # Given your birthday and the current date, calculate your age in days.
+
 # 1. Understanding a computational problem
 # Input: your birthday and the current date [Possible inputs (set)]
 # Output: your age in days(desired)
 # Solution: Input => [Procedure (relationship between inputs to outputs)] => Output
+
+# The first rule: What are the inputs?
+# Inputs: two dates
+# What is the set of valid inputs: 
+# (1)second date must not be before first date(Defensive Programming -> check this in our code)
+# (2) Gregorian calendar (15 Oct 1582)
+# How are inputs represented? => def daysBetweenDates(year1, month1, day1, year2, month2, day2)
