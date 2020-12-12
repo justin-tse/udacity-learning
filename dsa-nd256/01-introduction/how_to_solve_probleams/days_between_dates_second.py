1413de1e693afe77800a2a109797db6965cc9274
justin-tse <xiefahit@gmail.com>
2020-12-12 21:19:43 +0800

    Third rule: Work through some examples by hand

--- dsa-nd256/01-introduction/how_to_solve_probleams/days_between_dates_second.py
+++ dsa-nd256/01-introduction/how_to_solve_probleams/days_between_dates_second.py
@@ -17,3 +17,25 @@
 # Outputs: Calculate your age in days
 # How should we specify the output? => Return a number giving the numbers of days
 # between the first date and the second date.
+
+# Problem Statement:
+# Define a procedure, daysBetweenDates, that takes two dates as inputs and returns
+# the number of days between the first date and second date. Each date is passed in 
+# as three numbers giving a valid year, month, and day in the Gregorian calendar.
+# The second date must not be before the first.
+
+# Solving the Problem
+# Understand the Inputs, Understand the Outputs 
+# and Understand the Relationship - some examples => Consider systematically how a human solves the problem
+
+# The third rule: Work through some examples by hand
+# human algorithm pseudocode
+# days = #of days in month1 - day1
+# month1 += 1
+# while month1 < month2:
+#     days += #of days in month1
+#     month1 += 1
+# days += day2
+# while year1 < year2:
+#     days += days in year1
+# !!!! We should try to find a simple way !!!! Human is lazy to repeat but computer like mechnical alorithm.
