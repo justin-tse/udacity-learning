ab697f1c859b20195f4ed060521b656025392007
justin-tse <xiefahit@gmail.com>
2020-12-12 09:46:44 +0800

    Understanding a computational problem

--- dsa-nd256/01-introduction/how_to_solve_probleams/days_between_dates_second.py
+++ dsa-nd256/01-introduction/how_to_solve_probleams/days_between_dates_second.py
@@ -1,1 +1,6 @@
 # Days Between Dates
+# Given your birthday and the current date, calculate your age in days.
+# 1. Understanding a computational problem
+# Input: your birthday and the current date [Possible inputs (set)]
+# Output: your age in days(desired)
+# Solution: Input => [Procedure (relationship between inputs to outputs)] => Output
